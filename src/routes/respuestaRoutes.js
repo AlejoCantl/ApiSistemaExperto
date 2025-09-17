4178029152b34816be8f39990b5cbc9fd2196fc2
Alejo <alejandrocantillop.ilusa@gmail.com>
2025-09-17 11:15:09 -0500

    Endpoint para diagnostico del medico

--- src/routes/respuestaRoutes.js
+++ src/routes/respuestaRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const respuestaController = require('../controllers/respuestaController.js');
 
 router.post('/add', respuestaController.enviarRespuesta);
+router.post('/addWithoutSaving', respuestaController.enviarRespuestaSinGuardar);
 
 module.exports = router;
