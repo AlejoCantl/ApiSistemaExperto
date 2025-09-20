7f6402d9996dc340f8d948fee87efb9cee9fd70c
Alejo <alejandrocantillop.ilusa@gmail.com>
2025-09-20 15:22:11 -0500

    Vercel settings 1.06

--- api/index.js
+++ api/index.js
@@ -1,29 +1,10 @@
-const express = require('express');
-const serverLess = require('serverless-http');
-const cors = require('cors');
+const app = require('../app'); // Importa la app de Express del archivo principal
 
-const usuarioRoutes = require('../src/routes/usuarioRoutes.js');
-const respuestaRoutes = require('../src/routes/respuestaRoutes.js');
-const historialRoutes = require('../src/routes/historialRoutes.js');
-const preguntaRoutes = require('../src/routes/preguntaRoutes.js');
-
-const app = express();
-app.use(cors());
-
-//app.use(bodyparser.json());
-app.use(express.json());
-app.use('/usuario', usuarioRoutes);
-app.use('/respuesta', respuestaRoutes);
-app.use('/pregunta', preguntaRoutes);
-app.use('/historial', historialRoutes);
-
-module.exports = app;
-module.exports.handler = serverLess(app);
-
-// Si corres en local, levantar el server normalmente
-if (require.main === module) {
-  const PORT = 3000;
-  app.listen(PORT, () => {
-    console.log(`Servidor local escuchando en http://localhost:${PORT}`);
-  });
-}
+// Exporta como función de Vercel
+module.exports = (req, res) => {
+  // Agrega el prefijo /api a todas las rutas para Vercel
+  const originalUrl = req.url;
+  req.url = `/api${originalUrl}`;
+  
+  return app(req, res);
+};
